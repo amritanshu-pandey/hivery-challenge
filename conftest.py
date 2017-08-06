e62e7a075479f93ddd13f775194bad5fcb4c2b63
Amritanshu Pandey <amritanshu.aus@gmail.com>
2017-08-07 00:27:34 +1000

    Changes testcase framework to unittest

--- conftest.py
+++ conftest.py
@@ -1,6 +1,17 @@
 from app import APP
 import pytest
+import unittest
 
-@pytest.fixture
-def app():
-    return APP
+class FlaskTestCase(unittest.TestCase):
+    def test_positive_route_company(self):
+        tester = APP.test_client(self)
+        response = tester.get('/paranuara/company/COWTOWN', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_negative_route_company(self):
+        tester = APP.test_client(self)
+        response = tester.get('/paranuara/companIES/COWTOWN', content_type='application/json')
+        self.assertNotEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
